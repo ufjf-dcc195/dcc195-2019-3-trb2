94fc83a19786d6f5015134db121f9437fc9208cd
João Paulo <jpdias1997@hotmail.com>
2019-11-25 22:34:03 -0300

    Criação da Estrutura para inserção de sub unidades de uma unidade

--- app/controllers/unit.js
+++ app/controllers/unit.js
@@ -1,9 +1,11 @@
-
 const Moongoose = require('mongoose');
 const Unit = Moongoose.model("Unit");
 module.exports = {
-    createUnit
+    createUnit,
+    createSubUnits,
+    getAllUnits
 };
+
 function createUnit(req, res, next) {
     const unit = new Unit(req.body);
     unit.save((err) => {
@@ -13,4 +15,37 @@ function createUnit(req, res, next) {
             res.json(unit);
         }
     });
+}
+
+function createSubUnits(req, res, next) {
+    let parentUnit;
+    Unit.findOne({_id: req.params.unitId}).populate('filhos').exec(function (err,parentUnitFound) {
+        if (err) res.status(400).send(err.message);
+        parentUnit = parentUnitFound;
+    });
+    const unit = new Unit(req.body);
+    unit.save((err, unitSaved) => {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            parentUnit.filhos.push(unitSaved._id);
+            parentUnit.save(function (err, parentUnitSaved) {
+                if (err) res.status(400).send(err.message);
+                console.log(parentUnitSaved);
+            });
+            res.json(unit);
+        }
+    });
+}
+
+function getAllUnits(req, res, next) {
+    Unit.find()
+        .populate('filhos')
+        .exec(function (err, users) {
+            if (err) {
+                res.status(400).send(err.message);
+            } else {
+                res.json(users);
+            }
+        });
 }
--- app/routes/unit.js
+++ app/routes/unit.js
@@ -1,7 +1,8 @@
 let express = require('express');
 const router = express.Router();
-const UnitController = require('../controllers/unit')
+const UnitController = require('../controllers/unit');
 
 router.post('/new',UnitController.createUnit);
-
+router.post('/newSubUnit/:unitId',UnitController.createSubUnits);
+router.get('/', UnitController.getAllUnits);
 module.exports = router;
