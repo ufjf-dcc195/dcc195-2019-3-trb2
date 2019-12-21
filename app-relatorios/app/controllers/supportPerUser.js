25b2d88d36a8ac1193be66dff422fc13e96e6e63
J. Gabriel Maia <jonagabriel@gmail.com>
2019-12-20 21:57:52 -0300

    Adicionando relatorio de suporte por usuario

--- app-relatorios/app/controllers/supportPerUser.js
+++ app-relatorios/app/controllers/supportPerUser.js
@@ -6,6 +6,26 @@ const User = Moongoose.model("User");
 
 
 exports = function getReport(req, res, next) {
+    if(req.query.idAttendant) {
+        let idAttendant = req.query.idAttendant
+        let attendant = Attendant.findById(idAttendant)
+        let attendances = [Attendance.find({'atendente': attendant.nome})]
+        let users = []
+        attendances.forEach(attendance => {
+            users.push(attendance.usuario)
+        });
+        let units = [Unit.find({}, function(err) {
+            if (!err){ 
+                process.exit();
+            } else {throw err;}
+        })];
 
-
+        res.render('index', {
+            titulo: "Relatório",
+            usuarios: users,
+            atendente: attendant,
+            unidades: units
+        });
+    
+    }
  }
