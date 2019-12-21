9fb0089df9e70d4f6077bf89de943e372f42d79c
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-12-21 00:12:12 -0300

    Inserindo bootstrap e melhorando layout

--- backend/app/controllers/attendance.js
+++ backend/app/controllers/attendance.js
@@ -33,14 +33,6 @@ function qtdAtendimentoNivelDuvida(req, res, next) {
             if (it.nivel === 4) cont_4++
             if (it.nivel === 5) cont_5++
         })
-        // res.json({
-        // titulo: "Quantidade atendimento por nível dúvida",
-        // otima: cont_1,
-        // boa: cont_2,
-        // regular: cont_3,
-        // ruim: cont_4,
-        // pessima: cont_5
-        // });
 
         res.render('qtdNivelDuvida', {
             otima: cont_1,
@@ -49,6 +41,16 @@ function qtdAtendimentoNivelDuvida(req, res, next) {
             ruim: cont_4,
             pessima: cont_5
         })
+
+        // código de teste do backend
+        // res.json({
+        // titulo: "Quantidade atendimento por nível dúvida",
+        // otima: cont_1,
+        // boa: cont_2,
+        // regular: cont_3,
+        // ruim: cont_4,
+        // pessima: cont_5
+        // });
     })
 }
 
@@ -64,6 +66,16 @@ function qtdAtendimentoTipoDuvida(req, res, next) {
             if (it.duvidaFrequente === 4) cont_4++
             if (it.duvidaFrequente === 5) cont_5++
         })
+
+        res.render('qtdNivelTipoDuvida', {
+            cadastroPCDP: cont_1,
+            legislacao: cont_2,
+            relatorios: cont_3,
+            prestacaoContas: cont_4,
+            alteracaoPCDP: cont_5
+        })
+
+        // código de teste do backend
         // res.json({
         //     titulo: "Quantidade atendimento por tipo dúvida frequente",
         //     cadastroPCDP: cont_1,
@@ -72,13 +84,6 @@ function qtdAtendimentoTipoDuvida(req, res, next) {
         //     prestacaoContas: cont_4,
         //     alteracaoPCDP: cont_5
         // });
-        res.render('qtdNivelTipoDuvida', {
-            cadastroPCDP: cont_1,
-            legislacao: cont_2,
-            relatorios: cont_3,
-            prestacaoContas: cont_4,
-            alteracaoPCDP: cont_5
-        })
     })
 }
 
