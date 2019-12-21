9411474f68b0d79f6812cba670f116fd545337c9
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-12-20 23:44:01 -0300

    Encerrado front issue qtd atendimento nivel duvida

--- backend/app/controllers/attendance.js
+++ backend/app/controllers/attendance.js
@@ -1,11 +1,5 @@
 const moongoose = require('mongoose')
 const Attendance = moongoose.model("Attendance");
-const handlebars = require('express-handlebars')
-const express = require("express");
-const app = express()
-  
-app.engine('handlebars', handlebars({defaultLayout: 'index'}))
-app.set('view engine', 'handlebars')
 
 function getAllAttendance(req, res, next) {
     // const adm = new Attendance({
@@ -40,15 +34,21 @@ function qtdAtendimentoNivelDuvida(req, res, next) {
             if (it.nivel === 5) cont_5++
         })
         // res.json({
-        //     titulo: "Quantidade atendimento por nível dúvida",
-        //     otima: cont_1,
-        //     boa: cont_2,
-        //     regular: cont_3,
-        //     ruim: cont_4,
-        //     pessima: cont_5
+        // titulo: "Quantidade atendimento por nível dúvida",
+        // otima: cont_1,
+        // boa: cont_2,
+        // regular: cont_3,
+        // ruim: cont_4,
+        // pessima: cont_5
         // });
 
-        res.render('nivel')
+        res.render('qtdNivelDuvida', {
+            otima: cont_1,
+            boa: cont_2,
+            regular: cont_3,
+            ruim: cont_4,
+            pessima: cont_5
+        })
     })
 }
 
