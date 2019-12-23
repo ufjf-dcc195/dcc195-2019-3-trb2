409a7032df8bda8cbb4fd860c0506bc7e0f4183c
João Paulo <jpdias1997@hotmail.com>
2019-12-22 21:10:34 -0300

    Criação do Controller e Rotas de Atendentes para salvar, editar e deletar

--- backend/app/models/attendant.js
+++ backend/app/models/attendant.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const AttendantSchema = new Schema({
     nome: { type: String, required: true },
-    senha: { type: String, required: true }
+    senha: { type: String, required: true },
+    email: { type: String, required: true }
 });
 
 mongoose.model('Attendant', AttendantSchema);
--- backend/app/routes/session.js
+++ backend/app/routes/session.js
@@ -1,9 +1,8 @@
-let express = require('express')
-const router = express.Router()
-const sessionController = require('../controllers/session')
-const redirect = require('../controllers/session')
+let express = require('express');
+const router = express.Router();
+const sessionController = require('../controllers/session');
+const redirect = require('../controllers/session');
 
-router.get('/', sessionController.getAllAttendant)
-router.post('/login', redirect.redirectHome, sessionController.login)
-router.post('/logout', redirect.redirectLogin, sessionController.logout)    
+router.post('/login', redirect.redirectHome, sessionController.login);
+router.post('/logout', redirect.redirectLogin, sessionController.logout);
 module.exports = router;
